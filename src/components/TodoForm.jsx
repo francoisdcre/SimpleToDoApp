542ffc3bd95e0337e6f4beaefb345251b3bf1862
francoisdcre <francois.dcre@gmail.com>
2026-09-10 02:02:35 +0200

    Need to add a way to add item in todolist

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,22 @@
-function TodoForm({ currentTodoList }) {
+import { useState } from "react";
+
+function TodoForm({ currentTodo }) {
+  const [task, setTask] = useState("");
+
   return (
     <>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" placeholder="Element à ajouter"></input>
-        <button>Ajouter</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Element à ajouter"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        ></input>
+        <button type="submit">Ajouter</button>
       </form>
     </>
   );
